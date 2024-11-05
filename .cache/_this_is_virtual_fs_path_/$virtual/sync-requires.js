755d6e5f443cd16eb39c1292cc85a5b86a0ed1b4
ek05ywof <gitos.rrze.fau.de:mubashir.hassan@fau.de>
2024-11-05 22:33:50 +0100

    first website

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\mubas\\OneDrive\\Desktop\\portfolio\\Myportfolio\\.cache\\dev-404-page.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\Users\\mubas\\OneDrive\\Desktop\\portfolio\\Myportfolio\\src\\pages\\index.js"))
 }
 
